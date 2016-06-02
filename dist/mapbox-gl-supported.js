b3b67280aed36ce21af706433ec999c3aaf4f7be
danswick <dan.swick@gmail.com>
2016-06-01 18:19:20 -0700

    add dist build

---
+++ dist/mapbox-gl-supported.js
@@ -0,0 +1,125 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = isSupported;
+} else if (window) {
+    window.mapboxgl = window.mapboxgl || {};
+    window.mapboxgl.supported = isSupported;
+}
+
+/**
+ * Test whether the current browser supports Mapbox GL JS
+ * @param {Object} options
+ * @param {boolean} [options.failIfMajorPerformanceCaveat=false] Return `false`
+ *   if the performance of Mapbox GL JS would be dramatically worse than
+ *   expected (i.e. a software renderer is would be used)
+ * @return {boolean}
+ */
+function isSupported(options) {
+    return !!(
+        isBrowser() &&
+        isArraySupported() &&
+        isFunctionSupported() &&
+        isObjectSupported() &&
+        isJSONSupported() &&
+        isWorkerSupported() &&
+        isWebGLSupportedCached(options && options.failIfMajorPerformanceCaveat)
+    );
+}
+
+function isBrowser() {
+    return typeof window !== 'undefined' && typeof document !== 'undefined';
+}
+
+function isArraySupported() {
+    return (
+        Array.prototype &&
+        Array.prototype.every &&
+        Array.prototype.filter &&
+        Array.prototype.forEach &&
+        Array.prototype.indexOf &&
+        Array.prototype.lastIndexOf &&
+        Array.prototype.map &&
+        Array.prototype.some &&
+        Array.prototype.reduce &&
+        Array.prototype.reduceRight &&
+        Array.isArray
+    );
+}
+
+function isFunctionSupported() {
+    return Function.prototype && Function.prototype.bind;
+}
+
+function isObjectSupported() {
+    return (
+        Object.keys &&
+        Object.create &&
+        Object.getPrototypeOf &&
+        Object.getOwnPropertyNames &&
+        Object.isSealed &&
+        Object.isFrozen &&
+        Object.isExtensible &&
+        Object.getOwnPropertyDescriptor &&
+        Object.defineProperty &&
+        Object.defineProperties &&
+        Object.seal &&
+        Object.freeze &&
+        Object.preventExtensions
+    );
+}
+
+function isJSONSupported() {
+    return 'JSON' in window && 'parse' in JSON && 'stringify' in JSON;
+}
+
+function isWorkerSupported() {
+    return 'Worker' in window;
+}
+
+var isWebGLSupportedCache = {};
+function isWebGLSupportedCached(failIfMajorPerformanceCaveat) {
+
+    if (isWebGLSupportedCache[failIfMajorPerformanceCaveat] === undefined) {
+        isWebGLSupportedCache[failIfMajorPerformanceCaveat] = isWebGLSupported(failIfMajorPerformanceCaveat);
+    }
+
+    return isWebGLSupportedCache[failIfMajorPerformanceCaveat];
+}
+
+isSupported.webGLContextAttributes = {
+    antialias: false,
+    alpha: true,
+    stencil: true,
+    depth: true
+};
+
+function isWebGLSupported(failIfMajorPerformanceCaveat) {
+
+    var canvas = document.createElement('canvas');
+
+    var attributes = Object.create(isSupported.webGLContextAttributes);
+    attributes.failIfMajorPerformanceCaveat = failIfMajorPerformanceCaveat;
+
+    if (canvas.probablySupportsContext) {
+        return (
+            canvas.probablySupportsContext('webgl', attributes) ||
+            canvas.probablySupportsContext('experimental-webgl', attributes)
+        );
+
+    } else if (canvas.supportsContext) {
+        return (
+            canvas.supportsContext('webgl', attributes) ||
+            canvas.supportsContext('experimental-webgl', attributes)
+        );
+
+    } else {
+        return (
+            canvas.getContext('webgl', attributes) ||
+            canvas.getContext('experimental-webgl', attributes)
+        );
+    }
+}
+
+},{}]},{},[1]);
